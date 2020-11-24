3fe70b92f1521370fa9b24fbab79ccfc64258c79
bhattak <wraithsking@gmail.com>
2020-11-24 10:52:40 +0545

    Created new repo
    Contain a c++ file

---
+++ stack.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void check(){
+    stack<char> s;
+    s.push(a);
+    if(s.isempty()){
+        cout<<"Is Empty ";
+    }
+    else
+    {
+        cout<<"Not Empty ";
+    }
+}
+
+
